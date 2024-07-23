d18f027d417075fa0581e6ad50c203b6f713d241
foxblock <foxblock+github@gmail.com>
2024-07-23 20:24:04 +0200

    Minor improvements to scripts
    Better variable usage
    Get latest VS version
    Better options for zip creation

--- setup_build_c_vs.bat
+++ setup_build_c_vs.bat
@@ -1,7 +1,7 @@
 @echo OFF
 setlocal EnableDelayedExpansion
 
-rem Find CMake
+:: Find CMake
 where /q cmake.exe
 if ERRORLEVEL 1 (
 	echo Cannot find cmake.exe! 
@@ -11,23 +11,22 @@ if ERRORLEVEL 1 (
 	for /f "tokens=*" %%i in ('where cmake.exe') do set "CMAKE_PATH=%%i"
 )
 
-rem Find VS Install with C++ workload installed
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" (
-	for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -requires Microsoft.VisualStudio.Component.VC.CoreIde -property installationPath') do set "VS_PATH=%%i"
+set "VSWHERE_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+:: Find latest VS install with C++ workload (Component.VC.CoreIde)
+if exist "%VSWHERE_PATH%" (
+	for /f "tokens=*" %%i in ('"%VSWHERE_PATH%" -latest -requires Microsoft.VisualStudio.Component.VC.CoreIde -property installationPath') do set "VS_PATH=%%i"
 ) else (
-	rem vswhere not found, guessing Visual Studio install path.
-	set "VS_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community"
-	if not exist !VS_PATH!\* set "VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
+	:: vswhere not found, guessing Visual Studio install path.
+	set "VS_PATH=%ProgramFiles%\Microsoft Visual Studio\2022\Community"
+	if not exist !VS_PATH!\* set "VS_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community"
 )
 set "VCVARS64_PATH=%VS_PATH%\VC\Auxiliary\Build\vcvars64.bat"
 if not exist "%VCVARS64_PATH%" (
 	echo x64 Native Tools Command Prompt for VS ^(vcvars64.bat^) could not be found!
-	echo Make sure Visual Studio ^(2019 or 2022^) including the "Desktop development with C++" workload is installed
+	echo Make sure Visual Studio ^(2019 or 2022^) with the "Desktop development with C++" workload is installed
 	echo https://learn.microsoft.com/en-us/cpp/build/vscpp-step-0-installation?view=msvc-170
 	echo ^(DEBUG^) VCVARS64_PATH is "%VCVARS64_PATH%"
 	goto END
-) else (
-	set "VCVARS64_PATH=%VS_PATH%\VC\Auxiliary\Build\vcvars64.bat"
 )
 
 echo.
